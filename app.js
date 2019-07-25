b5ff32269a4ff036fe25e79f9905116cb1938bbc
zhouzhili <zhouzhili@360.cn>
2019-07-25 17:31:36 +0800

    修改打包方式，抽离frag绘图

---
+++ app.js
@@ -0,0 +1,71 @@
+import { GRender } from '@/utils/fragmentDraw'
+import glslLanguage from '@/utils/glsl-language'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+
+function initCodeEditor() {
+  monaco.languages.register({ id: 'glsl' })
+  monaco.languages.setMonarchTokensProvider('glsl', glslLanguage)
+
+  const monacoIns = monaco.editor.create(document.getElementById('code'), {
+    value: '',
+    language: 'glsl',
+    theme: 'vs-dark',
+    minimap: {
+      enabled: false
+    },
+    automaticLayout: true,
+  });
+  return monacoIns
+}
+
+function runCode() {
+  try {
+    const fragVal = monacoIns.getValue()
+    const enableTime = fragVal.indexOf('uniform float uTime;')
+    gRender.enableTime = enableTime !== -1
+    gRender.renderByShader(fragVal)
+  } catch (e) {
+    console.log(e)
+  }
+}
+function addEvent() {
+  const menuListEl = document.getElementById('menuList')
+
+  menuListEl.addEventListener('click', function(e) {
+    if (e.target.nodeName === 'LI') {
+      const { name } = e.target.dataset
+      if (name) {
+        initDraw(name)
+        const allActive = document.querySelectorAll('li.active')
+        for (let i = 0; i < allActive.length; i++) {
+          allActive[i].className = ''
+        }
+        e.target.className += 'active'
+      }
+    }
+  })
+
+  document.querySelector('#menuCtr').addEventListener('click', function(e) {
+    const menuStyle = menuListEl.style.display
+    menuListEl.style.display = menuStyle === 'none' ? 'block' : 'none'
+  })
+
+  document.querySelector('#runCode').addEventListener('click', runCode)
+}
+
+function initDraw(name) {
+  gRender.loadGLSL(`./fragments/${name}.glsl`).then(code => {
+    monacoIns.setValue(code)
+    runCode()
+  }).catch(err => {
+    console.log(`加载${name}.glsl失败`, err)
+  })
+}
+
+
+const monacoIns = initCodeEditor()
+const gRender = new GRender({
+  canvas: document.getElementById('gl-canvas')
+})
+addEvent()
+initDraw('coordinate')
